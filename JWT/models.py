0516438d95c60e1f0781afd1a14c8459262439fa
xitonchong <xitonchong@gmail.com>
2025-09-21 16:09:21 +0800

    JWT authentication

---
+++ JWT/models.py
@@ -0,0 +1,9 @@
+from database import Base 
+from sqlalchemy import Column, Integer, String 
+
+class Users(Base): 
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, index=True) 
+    username = Column(String, unique=True)
+    hashed_passwords = Column(String) 
